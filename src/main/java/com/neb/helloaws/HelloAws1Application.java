92504a41c93c625380ce1867d0f2016855cf8fff
techie-soft <techiesoft.business@gmail.com>
2026-08-05 21:50:56 +0530

    Initial Spring Boot application

---
+++ src/main/java/com/neb/helloaws/HelloAws1Application.java
@@ -0,0 +1,13 @@
+package com.neb.helloaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAws1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloAws1Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/neb/helloaws/HelloController.java
@@ -0,0 +1,18 @@
+package com.neb.helloaws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello AWS";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
